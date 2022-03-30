8543ff14171308b96a10326be22ccec505ee4a3a
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-03-30 17:08:00 +0900

    update table.rs

--- src/table.rs
+++ src/table.rs
@@ -21,9 +21,9 @@ impl SimpleTable {
     pub fn insert(&self, bufmgr: &mut BufferPoolManager, record: &[&[u8]]) -> Result<()> {
         let btree = BTree::new(self.meta_page_id);
         let mut key = vec![];
-        tuple::encode(record[..self.num_key_elems].iter(), bytes: &mut key);
-        let value = vec![];
-        tuple::encode(record[self.num_key_elems..].iter(), bytes: &mut value);
+        tuple::encode(record[..self.num_key_elems].iter(), &mut key);
+        let mut value = vec![];
+        tuple::encode(record[self.num_key_elems..].iter(), &mut value);
         btree.insert(bufmgr, &key, &value)?;
         Ok(())
     }
