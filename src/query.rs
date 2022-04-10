1ad43ebd24db7000b56cb9924fbeb5daea5bcebc
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-04-10 13:59:46 +0900

    クエリエクスキュータと実行計画の実装

---
+++ src/query.rs
@@ -0,0 +1,75 @@
+use anyhow::Result;
+
+use crate::btree::{self, BTree, SearchMode};
+use crate::buffer::BufferPoolManager;
+use crate::disk::PageId;
+use crate::tuple;
+
+pub type Tuple = Vec<Vec<u8>>;
+pub type TupleSlice<'a> = &'a [Vec<u8>];
+
+pub enum TupleSearchMode<'a> {
+    Start,
+    Key(&'a, [&'a [u8]]),
+}
+
+impl<'a> TupleSearchMode<'a> {
+    fn encode(&self) -> SearchMode {
+        match self {
+            TupleSearchMode::Start => SearchMode::Start,
+            TupleSearchMode::Key(tuple) => {
+                let mut key = vec![];
+                tuple::encode(tuple.iter(), &mut key);
+                SearchMode::Key(key)
+            }
+        }
+    }
+}
+
+pub trait Executor {
+    fn next(&mut self, bufmgr: &mut BufferPoolManager) -> Result<Option<Tuple>>;
+}
+
+pub type BoxExecuter<'a> = Box<dyn Executor + 'a>;
+
+pub trait PlanNode {
+    fn start(&self, bufmgr: &mut BufferPoolManager) -> Result<BoxExecuter>;
+}
+
+pub struct SeqScan<'a> {
+    pub table_meta_page_id: PageId,
+    pub search_mode: TupleSearchMode<'a>,
+    pub while_cond: &'a dyn Fn(TupleSlice) -> bool,
+}
+
+impl<'a> PlanNode for SeqScan<'a> {
+    fn start(&self, bufmgr: &mut BufferPoolManager) -> Result<BoxExecuter> {
+        let btree = BTree::new(self.table_meta_page_id);
+        let table_iter = btree.search(bufmgr, self.search_mode.encode())?;
+        Ok(Box::new(ExecSeqScan {
+            table_iter,
+            while_cond: self.while_cond,
+        }))
+    }
+}
+pub struct ExecSeqScan<'a> {
+    table_iter: btree::Iter,
+    while_cond: &'a dyn Fn(TupleSlice) -> bool,
+}
+
+impl<'a> Executor for ExecSeqScan<'a> {
+    fn next(&mut self, bufmgr: &mut BufferPoolManager) -> Result<Option<Tuple>> {
+        let (pkey_bytes, tuple_bytes) = match self.table_iter.next(bufmgr)? {
+            Some(pair) => pair,
+            None => return Ok(None),
+        };
+        let mut pkey = vec![];
+        tuple::decode(&pkey_bytes, &mut pkey);
+        if !(self.while_cond)(&pkey) {
+            return Ok(None);
+        }
+        let mut tuple = pkey;
+        tuple::decode(&tuple_bytes, &mut tuple);
+        Ok(Some(tuple))
+    }
+}
